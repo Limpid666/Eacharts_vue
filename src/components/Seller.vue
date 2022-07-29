51e7c3688e26146045a1b48888ea16279abe1b79
clear <limpid666@163.com>
2022-07-29 14:31:02 +0800

    feat(图表): 动态刷新的实现

--- src/components/Seller.vue
+++ src/components/Seller.vue
@@ -11,22 +11,49 @@ export default {
   data() {
     return {
       chartInstance: null,
-      allData: null
+      allData: null,
+      currentPage: 1,
+      totalPage: 0,
+      timerId: null
     }
+  },
+  destroyed() {
+    this.clearInterval(this.timerId)
   },
   methods: {
     initChart() {
       this.chartInstance = this.$echarts.init(this.$refs.sellerRef)
+      // 鼠标事件的监听
+      this.chartInstance.on('mouseover', () => {
+        clearInterval(this.timerId)
+      })
+      this.chartInstance.on('mouseout', () => {
+        this.startInterval()
+      })
     },
     async getData() {
       const { data: ret } = await this.$http.get('seller')
       this.allData = ret
+      this.allData.sort((a, b) => {
+        return a.value - b.value
+      })
+      this.totalPage =
+        this.allData % 5 === 0
+          ? this.allData.length / 5
+          : this.allData.length / 5 + 1
       console.log(ret)
       this.updataChart()
+      // 启动定时器
+      this.startInterval()
     },
     updataChart() {
-      const sellerNames = this.allData.map((item) => item.name)
-      const sellerValues = this.allData.map((item) => item.value)
+      // 动态刷新
+      const start = (this.currentPage - 1) * 5
+      const end = this.currentPage * 5
+      const showData = this.allData.slice(start, end)
+      // 基础实现
+      const sellerNames = showData.map((item) => item.name)
+      const sellerValues = showData.map((item) => item.value)
       const option = {
         xAxis: {
           type: 'value'
@@ -38,6 +65,18 @@ export default {
         series: [{ type: 'bar', data: sellerValues }]
       }
       this.chartInstance.setOption(option)
+    },
+    startInterval() {
+      if (this.timerId) {
+        clearInterval(this.timerId)
+      }
+      this.timerId = setInterval(() => {
+        this.currentPage++
+        if (this.currentPage > this.totalPage) {
+          this.currentPage = 1
+        }
+        this.updataChart()
+      }, 3000)
     }
   },
   created() {},
